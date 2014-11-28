54b3d2f21648b2efd4891ad464561d872e1b062e
ccleung <clemsquared@gmail.com>
2014-11-27 22:44:41 -0500

    reddit is zero based pagination

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -8,7 +8,7 @@ redditPicsControllers.controller('RedditPicsCtrl', ['$scope', '$http',
 
   function($scope, $http) {
   	$http.defaults.headers.common.Authorization = "Client-ID 46ffdf043d06c64";
-    $http.get('https://api.imgur.com/3/gallery/r/aww/top/day/1').success(function(data) {
+    $http.get('https://api.imgur.com/3/gallery/r/aww/top/day/0').success(function(data) {
       $scope.imgs = data.data;
     });
   }]).directive('imageonload', function() {
