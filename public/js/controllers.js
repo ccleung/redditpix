34860fa06d5a78b8ffe9ae1a5e5a3dba73fd72b6
ccleung <clemsquared@gmail.com>
2014-11-27 22:24:37 -0500

    custom directive to detect when image has been loaded. When image is loaded, show our image with animation

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -5,9 +5,19 @@
 var redditPicsControllers = angular.module('redditPicsControllers', []);
 
 redditPicsControllers.controller('RedditPicsCtrl', ['$scope', '$http',
+
   function($scope, $http) {
   	$http.defaults.headers.common.Authorization = "Client-ID 46ffdf043d06c64";
     $http.get('https://api.imgur.com/3/gallery/r/aww/top/day/1').success(function(data) {
       $scope.imgs = data.data;
     });
-  }]);
+  }]).directive('imageonload', function() {
+    return {
+        restrict: 'A',
+        link: function(scope, element, attrs) {
+            element.bind('load', function() {
+              scope.$apply('imageLoaded=true');
+            });
+        }
+    };
+});
