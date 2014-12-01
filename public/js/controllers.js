7c1d6e2a5a10cfbdfc2dd3b7c26656ee786c103c
ccleung <clemsquared@gmail.com>
2014-11-30 21:17:24 -0500

    don't hang if picture fails to load. show a spinner when pictures are loading

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -2,7 +2,7 @@
 
 /* Controllers */
 
-var redditPicsControllers = angular.module('redditPicsControllers', ['infinite-scroll']);
+var redditPicsControllers = angular.module('redditPicsControllers', ['infinite-scroll','angularSpinner']);
 
 redditPicsControllers.controller('RedditPicsCtrl', ['$scope', 'Pictures',
 
@@ -11,35 +11,45 @@ redditPicsControllers.controller('RedditPicsCtrl', ['$scope', 'Pictures',
   }]);
 
 redditPicsControllers.directive('imageonload', function() {
+
     return {
         restrict: 'A',
         link: function(scope, element, attrs) {
-            element.bind('load', function() {
-
+            var onLoadOrError = function() {
               // only allow more scrolling when all pictures are loaded
               scope.pictures.imagesLoaded++;
               if (scope.pictures.imagesLoaded >= scope.pictures.imgs.length)
               {
-                console.log("NOT BUSY");
                 scope.pictures.busy = false;
+                scope.pictures.stopSpinner();
               }
               scope.$apply('imageLoaded=true');
+            }
+            element.bind('load', function() {
+              onLoadOrError();
+            });
+            element.bind('error', function() {
+              onLoadOrError();
             });
         }
     };
+
 });
 
-redditPicsControllers.factory('Pictures', function($http) {
+redditPicsControllers.factory('Pictures', ['$http', 'usSpinnerService', function($http, usSpinnerService) {
   var Pictures = function() {
     this.imgs = [];
     this.busy = false;
     this.index = 0;
     this.imagesLoaded = 0;
+    this.startSpinner = function() { return usSpinnerService.spin('spinner-1'); }
+    this.stopSpinner = function() { return usSpinnerService.stop('spinner-1'); }
   };
 
   Pictures.prototype.nextPage = function() {
     if (this.busy) return;
     this.busy = true;
+    this.startSpinner();
 
     var url = "https://api.imgur.com/3/gallery/r/aww/top/day/" + this.index;
     $http.defaults.headers.common.Authorization = "Client-ID 46ffdf043d06c64";
@@ -53,4 +63,4 @@ redditPicsControllers.factory('Pictures', function($http) {
   };
 
   return Pictures;
-});
+}]);
